45dae550971741f60054f640e71955ad7b17da47
itss0urav <sourav2021official@gmail.com>
2023-06-20 21:23:29 +0530

    updates

--- client/src/components/contact.jsx
+++ client/src/components/contact.jsx
@@ -1,70 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Navbar from "./navbar";
 
 const ContactUs = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Message:', message);
-    setName('');
-    setEmail('');
-    setMessage('');
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", message);
+    setName("");
+    setEmail("");
+    setMessage("");
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="name" className="block font-bold mb-1">
-            Name:
-          </label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block font-bold mb-1">
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="message" className="block font-bold mb-1">
-            Message:
-          </label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded resize-y"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="px-4 py-2 bg-gray-800 text-white rounded cursor-pointer"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+    <>
+    <Navbar/>
+      <div className="container mx-auto p-4">
+        <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block font-bold mb-1">
+              Name:
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block font-bold mb-1">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block font-bold mb-1">
+              Message:
+            </label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded resize-y"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-gray-800 text-white rounded cursor-pointer"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
