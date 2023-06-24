bc46d90c4cde9667531950a6a7794bdc5ffe40b5
itss0urav <sourav2021official@gmail.com>
2023-06-24 12:08:22 +0530

    updates

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -8,6 +8,7 @@ const Login = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
 
   useEffect(() => {
     const isLoggedIn = localStorage.getItem("isLoggedIn");
@@ -19,6 +20,8 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
+
+    setIsLoggingIn(true);
 
     try {
       const response = await axios.post(
@@ -39,41 +42,61 @@ const Login = () => {
       navigate("/"); // Redirect to the home page
     } catch (error) {
       console.error("Login failed:", error);
+      setIsLoggingIn(false);
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h2 className="text-2xl mb-4">Login</h2>
-      <form className="mb-4" onSubmit={handleLogin}>
-        <input
-          className="border border-gray-400 rounded py-2 px-4 mb-2 w-64 sm:w-auto"
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          className="border border-gray-400 rounded py-2 px-4 mb-2 w-64 sm:w-auto"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          type="submit"
-        >
-          Login
-        </button>
-      </form>
-      <div className="text-center">
-        New here?
-        <Link to="/register">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
-            Register
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-6">Login</h2>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-700 font-medium mb-2">
+              Username
+            </label>
+            <input
+              id="username"
+              className="border border-gray-300 rounded py-2 px-3 w-full"
+              type="text"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
+              Password
+            </label>
+            <input
+              id="password"
+              className="border border-gray-300 rounded py-2 px-3 w-full"
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full transition-opacity ${
+              isLoggingIn ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            type="submit"
+            disabled={isLoggingIn}
+          >
+            {isLoggingIn ? "Logging In..." : "Login"}
           </button>
-        </Link>
+        </form>
+        <div className="mt-4 text-center">
+          <p className="text-gray-600">
+            New here?{" "}
+            <Link to="/register" className="text-blue-500 hover:text-blue-700">
+              Register
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
