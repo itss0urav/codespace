bb446254dfe700f92e9e28954015930355b60bdd
itss0urav <sourav2021official@gmail.com>
2023-06-29 13:53:11 +0530

    updates

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
@@ -6,6 +6,7 @@ const Navbar = () => {
   const { isAuthenticated, setIsAuthenticated, username } =
     useContext(AuthContext);
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleLogout = () => {
     // Perform logout logic here...
@@ -19,6 +20,10 @@ const Navbar = () => {
     localStorage.removeItem("isLoggedIn");
   };
 
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav className="bg-gray-800">
       <div className="container mx-auto px-4">
@@ -29,50 +34,119 @@ const Navbar = () => {
             </Link>
           </div>
 
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4">
-              <NavLink
-                exact
-                to="/"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                activeClassName="bg-gray-900 text-white"
+          {/* Mobile Menu */}
+          <div className="md:hidden">
+            <button
+              className="text-gray-300 hover:text-white focus:outline-none focus:text-white"
+              onClick={toggleMenu}
+            >
+              <svg
+                className="h-6 w-6 fill-current"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                Home
-              </NavLink>
-              <NavLink
-                to="/about"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                activeClassName="bg-gray-900 text-white"
-              >
-                About Us
-              </NavLink>
-              <NavLink
-                to="/contact"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                activeClassName="bg-gray-900 text-white"
-              >
-                Contact Us
-              </NavLink>
-            </div>
+                <path
+                  className={showMenu ? "hidden" : ""}
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M3 5h18v1H3V5zm0 7h18v-1H3v1zm0 4h18v-1H3v1z"
+                />
+                <path
+                  className={showMenu ? "" : "hidden"}
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 6h16v1H4V6zm0 5h16v1H4v1zm0 5h16v1H4v1z"
+                />
+              </svg>
+            </button>
           </div>
 
-          <div className="hidden md:block">
+          {/* Desktop Menu */}
+          <div className="hidden md:flex md:items-center md:ml-10">
+            <NavLink
+              exact
+              to="/"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              activeClassName="bg-gray-900 text-white"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/about"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              activeClassName="bg-gray-900 text-white"
+            >
+              About Us
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              activeClassName="bg-gray-900 text-white"
+            >
+              Contact Us
+            </NavLink>
             {isAuthenticated ? (
-              <>
-                <div className="flex items-center">
-                  <p className="text-gray-300 pr-3">{username}</p>
-                </div>
+              <div className="flex items-center ml-6 space-x-4">
+                <p className="text-gray-300">{username}</p>
                 <button
                   onClick={handleLogout}
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Logout
                 </button>
-              </>
+              </div>
             ) : (
               <Link
                 to="/login"
                 className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+
+        {/* Mobile Menu */}
+        <div
+          className={`md:hidden ${showMenu ? "block" : "hidden"}`}
+        >
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            <NavLink
+              exact
+              to="/"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              activeClassName="bg-gray-900 text-white"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/about"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              activeClassName="bg-gray-900 text-white"
+            >
+              About Us
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              activeClassName="bg-gray-900 text-white"
+            >
+              Contact Us
+            </NavLink>
+            {isAuthenticated ? (
+              <div className="pl-3">
+                <p className="text-gray-300">{username}</p>
+                <button
+                  onClick={handleLogout}
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <Link
+                to="/login"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium"
               >
                 Login
               </Link>
