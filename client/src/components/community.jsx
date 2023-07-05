4ef8d16c103a64fc7c64a352bb4cafff8dbb783c
itss0urav <sourav2021official@gmail.com>
2023-07-05 19:06:38 +0530

    updates

--- client/src/components/community.jsx
+++ client/src/components/community.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import Navbar from "./navbar";
+import { Link } from "react-router-dom";
 
-function community() {
+function Community() {
   return (
     <div>
       <Navbar />
-      <div className="bg-gray-400 m-2 p-2">
-        <div className=" bg-blue-700 text-white text-lg w-20 text-center rounded-lg mt-2 hover:text-black hover:bg-blue-600 ">
-          Threads
-        </div>
-        <div className=" bg-blue-700 text-white text-lg w-20 text-center rounded-lg mt-2 hover:text-black hover:bg-blue-600 ">
-          Updates
-        </div>
-        <div className=" bg-blue-700 text-white text-lg w-20 text-center rounded-lg mt-2 hover:text-black hover:bg-blue-600 ">
-          Donate
-        </div>
+      <div className="bg-gray-400 m-2 p-2 flex flex-wrap justify-center">
+        <Link to="/Threads">
+          <div className="bg-blue-700 text-white text-lg w-28 text-center rounded-lg m-2 py-2 hover:bg-blue-600 transition-colors duration-300">
+            Threads
+          </div>
+        </Link>
+        <Link to="/Updates">
+          <div className="bg-blue-700 text-white text-lg w-28 text-center rounded-lg m-2 py-2 hover:bg-blue-600 transition-colors duration-300">
+            Updates
+          </div>
+        </Link>
+        <Link to="/Donate">
+          <div className="bg-blue-700 text-white text-lg w-28 text-center rounded-lg m-2 py-2 hover:bg-blue-600 transition-colors duration-300">
+            Donate
+          </div>
+        </Link>
       </div>
     </div>
   );
 }
 
-export default community;
+export default Community;
