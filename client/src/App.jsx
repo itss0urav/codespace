0da685c667288ed11c77f00306a2139abe329cf5
itss0urav <sourav2021official@gmail.com>
2023-07-06 08:48:05 +0530

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
 import React, { useContext } from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Home from "./components/Home";
-import About from "./components/about";
-import ContactUs from "./components/contact";
+
 import { AuthContext } from "./context/AuthContext"; // Update the import statement
-import Html from "./components/html";
+
+//components
+
 import HTMLTutorials from "./components/htmlTutorials";
-import Css from "./components/css";
 import CssTutorials from "./components/cssTutorials";
 import Javascript from "./components/javascript";
 import ReactPage from "./components/reactPage";
 import Community from "./components/community";
+import Register from "./components/Register";
+import ContactUs from "./components/contact";
+import Donate from "./components/donate";
+import Login from "./components/Login";
+import About from "./components/about";
+import Home from "./components/Home";
+import Html from "./components/html";
+import Css from "./components/css";
 function App() {
   const { isAuthenticated } = useContext(AuthContext); // Access the isAuthenticated state
 
@@ -24,17 +30,18 @@ function App() {
           path="/"
           element={isAuthenticated ? <Home /> : <Login />} // Render the Home component if authenticated, otherwise render the Login component
         />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/contact" element={<ContactUs />} />
-        <Route exact path="/html" element={<Html />} />
         <Route exact path="/htmltutorials" element={<HTMLTutorials />} />
-        <Route exact path="/css" element={<Css />} />
         <Route exact path="/csstutorials" element={<CssTutorials />} />
         <Route exact path="/javascript" element={<Javascript />} />
         <Route exact path="/reactPage" element={<ReactPage />} />
         <Route exact path="/community" element={<Community />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/contact" element={<ContactUs />} />
+        <Route exact path="/Donate" element={<Donate />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/html" element={<Html />} />
+        <Route exact path="/css" element={<Css />} />
       </Routes>
     </Router>
   );
---
+++ client/src/components/donate.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./navbar";
+
+function donate() {
+  return (
+    <div>
+      <Navbar />
+      donate
+    </div>
+  );
+}
+
+export default donate;
